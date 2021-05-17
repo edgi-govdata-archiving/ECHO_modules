d51f10de35997c1d87567ae47cad8ed0ad976a05
Eric Nost <ericnost@users.noreply.github.com>
2021-05-17 17:27:37 -0400

    Update utilities.py
    testing how we might reorganize utility functions in light of creating an Echo class that embeds many functions as class methods

--- utilities.py
+++ utilities.py
@@ -10,36 +10,41 @@ import csv
 import datetime
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import folium
-import seaborn as sns
-from folium.plugins import FastMarkerCluster
 import ipywidgets as widgets
 from ipywidgets import interact, interactive, fixed, interact_manual, Layout
-from ECHO_modules.DataSet import get_data
-from ECHO_modules.geographies import region_field, states
-
 from IPython.display import display
 
-
-# Set up some default parameters for graphing
-from matplotlib import cycler
-colour = "#00C2AB" # The default colour for the barcharts
-colors = cycler('color',
-                ['#4FBBA9', '#E56D13', '#D43A69',
-                 '#25539f', '#88BB44', '#FFBBBB'])
-plt.rc('axes', facecolor='#E6E6E6', edgecolor='none',
-       axisbelow=True, grid=True, prop_cycle=colors)
-plt.rc('grid', color='w', linestyle='solid')
-plt.rc('xtick', direction='out', color='gray')
-plt.rc('ytick', direction='out', color='gray')
-plt.rc('patch', edgecolor='#E6E6E6')
-plt.rc('lines', linewidth=2)
-font = {'family' : 'DejaVu Sans',
-        'weight' : 'normal',
-        'size'   : 16}
-plt.rc('font', **font)
-plt.rc('legend', fancybox = True, framealpha=1, shadow=True, borderpad=1)
+def get_data( sql, index_field=None ):
+    '''
+    This is the global function that can run an SQL query against
+    the database and return the resulting Pandas DataFrame.
+    Parameters
+    ----------
+    sql : str
+        The SQL query to run
+    index_field : str
+        The field in the result set to set as the Dataframe's index
+    Results
+    -------
+    Dataframe
+        The results of the database query
+    '''    
+    url= 'http://portal.gss.stonybrook.edu/echoepa/?query=' #'http://apps.tlt.stonybrook.edu/echoepa/?query=' 
+    data_location=url+urllib.parse.quote_plus(sql) + '&pg'
+    # print( sql )
+    # print( data_location )
+    if ( index_field == "REGISTRY_ID" ):
+        ds = pd.read_csv(data_location,encoding='iso-8859-1', 
+                 dtype={"REGISTRY_ID": "Int64"})
+    else:
+        ds = pd.read_csv(data_location,encoding='iso-8859-1')
+    if ( index_field is not None ):
+        try:
+            ds.set_index( index_field, inplace=True)
+        except KeyError:
+            pass
+    # print( "get_data() returning {} records.".format( len(ds) ))
+    return ds
 
 
 def fix_county_names( in_counties ):
@@ -218,225 +223,6 @@ def show_fac_widget( fac_series ):
     return widget
 
 
-def get_active_facilities( state, region_type, region_selected ):
-    '''
-    Get a Dataframe with the ECHO_EXPORTER facilities with FAC_ACTIVE_FLAG
-    set to 'Y' for the region selected.
-
-    Parameters
-    ----------
-    state : str
-        The state, which could be None
-    region_type : str
-        The type of region:  'State', 'Congressional District', etc.
-    region_selected : str
-        The selected region of the specified region_type
-
-    Returns
-    -------
-    Dataframe
-        The active facilities returned from the database query
-    '''
-
-    if ( region_type == 'State' ):
-        sql = 'select * from "ECHO_EXPORTER" where "FAC_STATE" = \'{}\''
-        sql += ' and "FAC_ACTIVE_FLAG" = \'Y\''
-        sql = sql.format( state )
-        df_active = get_data( sql, 'REGISTRY_ID' )
-    elif ( region_type == 'Congressional District'):
-        sql = 'select * from "ECHO_EXPORTER" where "FAC_STATE" = \'{}\''
-        sql += ' and "FAC_DERIVED_CD113" = \'{}\''
-        sql += ' and "FAC_ACTIVE_FLAG" = \'Y\''
-        sql = sql.format( state, region_selected )
-        df_active = get_data( sql, 'REGISTRY_ID' )
-    elif ( region_type == 'County' ):
-        sql = 'select * from "ECHO_EXPORTER" where "FAC_STATE" = \'{}\''
-        sql += ' and "FAC_COUNTY" = \'{}\''
-        sql += ' and "FAC_ACTIVE_FLAG" = \'Y\''
-        sql = sql.format( state, region_selected )
-        df_active = get_data( sql, 'REGISTRY_ID' )
-    else:  ## Zip code
-        sql = 'select * from "ECHO_EXPORTER" where "FAC_ZIP" = \'{}\''
-        sql += ' and "FAC_ACTIVE_FLAG" = \'Y\''
-        sql = sql.format( region_selected )
-        df_active = get_data( sql, 'REGISTRY_ID' )
-    return df_active
-
-
-def marker_text( row, no_text ):
-    '''
-    Create a string with information about the facility or program instance.
-
-    Parameters
-    ----------
-    row : Series
-        Expected to contain FAC_NAME and DFR_URL fields from ECHO_EXPORTER
-    no_text : Boolean
-        If True, don't put any text with the markers, which reduces chance of errors 
-
-    Returns
-    -------
-    str
-        The text to attach to the marker
-    '''
-
-    text = ""
-    if ( no_text ):
-        return text
-    if ( type( row['FAC_NAME'] == str )) :
-        try:
-            text = row["FAC_NAME"] + ' - '
-        except TypeError:
-            print( "A facility was found without a name. ")
-        #text += " - <p><a href='"+row["DFR_URL"]
-        #text += "' target='_blank'>Link to ECHO detailed report</a></p>" 
-	#Somehow DFR_URL got dropped from the views, so asking for it here ^ is breaking def mapper() 
-    return text
-
-
-def check_bounds( row, bounds ):
-    '''
-    See if the FAC_LAT and FAC_LONG of the row are interior to
-    the minx, miny, maxx, maxy of the bounds.
-
-    Parameters
-    ----------
-    row : Series
-	Must contain FAC_LAT and FAC_LONG
-    bounds : Dataframe
-	Bounding rectangle--minx,miny,maxx,maxy
-
-    Returns
-    -------
-    True if the row's point is in the bounds
-    '''
-
-    if ( row['FAC_LONG'] < bounds.minx[0] or row['FAC_LAT'] < bounds.miny[0] \
-         or row['FAC_LONG'] > bounds.maxx[0] or row['FAC_LAT'] > bounds.maxy[0]):
-        return False
-    return True
-
-
-def mapper(df, bounds=None, no_text=False):
-    '''
-    Display a map of the Dataframe passed in.
-    Based on https://medium.com/@bobhaffner/folium-markerclusters-and-fastmarkerclusters-1e03b01cb7b1
-
-    Parameters
-    ----------
-    df : Dataframe
-        The facilities to map.  They must have a FAC_LAT and FAC_LONG field.
-    bounds : Dataframe
-        A bounding rectangle--minx, miny, maxx, maxy.  Discard points outside.
-
-    Returns
-    -------
-    folium.Map
-    '''
-
-    # Initialize the map
-    m = folium.Map(
-        location = [df.mean()["FAC_LAT"], df.mean()["FAC_LONG"]]
-    )
-
-    # Create the Marker Cluster array
-    #kwargs={"disableClusteringAtZoom": 10, "showCoverageOnHover": False}
-    mc = FastMarkerCluster("")
- 
-    # Add a clickable marker for each facility
-    for index, row in df.iterrows():
-        if ( bounds is not None ):
-            if ( not check_bounds( row, bounds )):
-                continue
-        mc.add_child(folium.CircleMarker(
-            location = [row["FAC_LAT"], row["FAC_LONG"]],
-            popup = marker_text( row, no_text ),
-            radius = 8,
-            color = "black",
-            weight = 1,
-            fill_color = "orange",
-            fill_opacity= .4
-        ))
-    
-    m.add_child(mc)
-    bounds = m.get_bounds()
-    m.fit_bounds(bounds)
-
-    # Show the map
-    return m
-
-def point_mapper(df, aggcol, quartiles=False, other_fac=None):
-  '''
-  Display a point symbol map of the Dataframe passed in. A point symbol map represents 
-  each facility as a point, with the size of the point scaled to the data value 
-  (e.g. inspections, violations) proportionally or through quartiles.
-  Parameters
-  ----------
-  df : Dataframe
-      The facilities to map. They must have a FAC_LAT and FAC_LONG field.
-      This Dataframe should
-      already be aggregated by facility e.g.:
-      NPDES_ID  violations  FAC_LAT FAC_LONG
-      NY12345   13          43.03   -73.92
-      NY54321   2           42.15   -80.12
-      ...
-  aggcol : String
-      The name of the field in the Dataframe that has been aggregated. This is
-      used for the legend (pop-up window on the map)
-  quartiles : Boolean
-      False (default) returns a proportionally-scaled point symbol map, meaning
-      that the radius of each point is directly scaled to the value (e.g. 13 violations)
-      True returns a graduated point symbol map, meaning that the radius of each 
-      point is a function of the splitting the Dataframe into quartiles. 
-  other_fac : Dataframe
-      Other regulated facilities without violations, inspections,
-      penalties, etc. - whatever the value being mapped is. This is an optional 
-      variable enabling further context to the map. They must have a FAC_LAT and FAC_LONG field.
-  Returns
-  -------
-  folium.Map
-  '''
-  if ( df is not None ):
-
-    map_of_facilities = folium.Map()
-   
-    if quartiles == True:
-      df['quantile'] = pd.qcut(df[aggcol], 4, labels=False, duplicates="drop")
-      scale = {0: 8,1:12, 2: 16, 3: 24} # First quartile = size 8 circles, etc.
-
-    # Add a clickable marker for each facility
-    for index, row in df.iterrows():
-      if quartiles == True:
-        r = scale[row["quantile"]]
-      else:
-        r = row[aggcol]
-      map_of_facilities.add_child(folium.CircleMarker(
-          location = [row["FAC_LAT"], row["FAC_LONG"]],
-          popup = aggcol+": "+str(row[aggcol]),
-          radius = r * 4, # arbitrary scalar
-          color = "black",
-          weight = 1,
-          fill_color = "orange",
-          fill_opacity= .4
-      ))
-    
-    if ( other_fac is not None ):
-      for index, row in other_fac.iterrows():
-        map_of_facilities.add_child(folium.CircleMarker(
-            location = [row["FAC_LAT"], row["FAC_LONG"]],
-            popup = "other facility",
-            radius = 4,
-            color = "black",
-            weight = 1,
-            fill_color = "black",
-            fill_opacity= 1
-        ))
-
-    return map_of_facilities
-
-  else:
-    print( "There are no facilities to map." ) 
-
 def write_dataset( df, base, type, state, region ):
     '''
     Write out a file of the Dataframe passed in.
@@ -516,86 +302,3 @@ def make_filename( base, type, state, region, filetype='csv' ):
     if ( not os.path.exists( dir )):
         os.makedirs( dir )
     return dir + filename
-
-
-def get_top_violators( df_active, flag, state, cd, noncomp_field, action_field, num_fac=10 ):
-    '''
-    Sort the dataframe and return the rows that have the most number of
-    non-compliant quarters.
-
-    Parameters
-    ----------
-    df_active : Dataframe
-        Must have ECHO_EXPORTER fields
-    flag : str
-        Identifies the EPA programs of the facility (AIR_FLAG, NPDES_FLAG, etc.)
-    state : str
-        The state
-    cd : str
-        The congressional district    
-    noncomp_field : str
-        The field with the non-compliance values, 'S' or 'V'.
-    action_field
-        The field with the count of quarters with formal actions
-    num_fac
-        The number of facilities to include in the returned Dataframe
-
-    Returns
-    -------
-    Dataframe
-        The top num_fac violators for the EPA program in the region
-
-    Examples
-    --------
-    >>> df_violators = get_top_violators( df_active, 'AIR_FLAG', state, region_selected, 
-        'CAA_3YR_COMPL_QTRS_HISTORY', 'CAA_FORMAL_ACTION_COUNT', 20 )
-    '''
-    df = df_active.loc[ df_active[flag] == 'Y' ]
-    if ( len( df ) == 0 ):
-        return None
-    df_active = df.copy()
-    noncomp = df_active[ noncomp_field ]
-    noncomp_count = noncomp.str.count('S') + noncomp.str.count('V')
-    df_active['noncomp_count'] = noncomp_count
-    df_active = df_active[['FAC_NAME', 'noncomp_count', action_field,
-            'DFR_URL', 'FAC_LAT', 'FAC_LONG']]
-    df_active = df_active.sort_values( by=['noncomp_count', action_field], 
-            ascending=False )
-    df_active = df_active.head( num_fac )
-    return df_active   
-
-def chart_top_violators( ranked, state, cd, epa_pgm ):
-    '''
-    Draw a horizontal bar chart of the top non-compliant facilities.
-
-    Parameters
-    ----------
-    ranked : Dataframe
-        The facilities to be charted
-    state : str
-        The state
-    cd : integer
-        The Congressional District
-    epa_pgm : str
-        The EPA program associated with this list of non-compliant facilities
-
-    Returns
-    -------
-    seaborn.barplot
-        The graph that is generated
-    '''
-    sns.set(style='whitegrid')
-    fig, ax = plt.subplots(figsize=(20,30))
-    unit = ranked.index 
-    values = ranked['noncomp_count'] 
-    try:
-        g = sns.barplot(values, unit, order=list(unit), orient="h") 
-        g.set_title('{} facilities with the most non-compliant quarters in {} - {}'.format( 
-                epa_pgm, state, str( cd )))
-        ax.set_xlabel("Non-compliant quarters")
-        ax.set_ylabel("Facility")
-        ax.set_yticklabels(ranked["FAC_NAME"])
-        return ( g )
-    except TypeError as te:
-        print( "TypeError: {}".format( str(te) ))
-        return None
