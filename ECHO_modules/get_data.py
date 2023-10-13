a8d2bf17aec0fc866c8e8a3d0dfd73b3350376ec
Eric Nost <ericnost@users.noreply.github.com>
2023-10-13 18:11:33 -0400

    better mapping
    `get_spatial_data()` gets spatial data from the SBU database (in some cases straight from the Census too) 
    `show_regions()` shows a map of regions selected from get_spatial_data() 
    `bivariate_map()` shows a map of regions + point data (industrial facilities)

--- ECHO_modules/get_data.py
+++ ECHO_modules/get_data.py
@@ -1,5 +1,5 @@
 import pdb
-
+import geopandas
 import os
 import urllib.parse
 import pandas as pd
@@ -61,57 +61,85 @@ def selector(units):
         selection = '(\''+str(units)+'\')'
     return selection
 
-def get_spatial_data(intersection, other, units):
+def spatial_selector(units):
     '''
-    # return query(unit, unit_type)
-    #places = more specifically, the zip code(s). Can provide a list of them. 
-    #at least one place is required (can't query the whole db!)
+    helper function for `get_spatial_data`
+    helps parse out multiple inputs into a SQL format
+    e.g. takes a list ["AL", "AK", "AR"] and returns the string ("AL", "AK", "AR")
+    '''
+    selection = '('
+    if (type(units) == list):
+      for place in units:
+          selection += '\''+str(place)+'\', '
+      selection = selection[:-2] # remove trailing comma
+      selection += ')'
+    else:
+      selection = '(\''+str(units)+'\')'
+    return selection
 
-    #return spatial data set based on the intersection between one SDS and another 
-    #e.g. return watersheds based on what states they cross
-    #When intersection is set to true places should equal a location(s) from another
-    #spatial data set (other)
+def get_spatial_data(region_type, states, spatial_tables, fips=None, region_filter=None):
+    '''
+    returns spatial data from the database utilizing an intersection query 
+    e.g. return watersheds based on whether they cross the selected state
+
+    region_type = "Congressional District" # from cell 3 region_type_widget
+    states = ["AL"]  # from cell 2 state dropdown selection. 
+    states variable has ability to be expanded to multiple state selection.
+    spatial_tables is from ECHO_modules/geographies.py
+    fips is also from geographies.py but is not required - only for Census Tracts
+    region_filter can specify whether to return a single unit (e.g. a specific county)
+    region_filter should be based on the id_field specified in spatial_tables
     '''
 
-    selection = selector()
+    def retrieve(query):
+      '''
+      Actually gets the data...
+      '''
+      print(query) # Debugging
+      result = get_echo_data(query)
+      result['geometry'] = geopandas.GeoSeries.from_wkb(result['wkb_geometry'])
+      result.drop("wkb_geometry", axis=1, inplace=True)
+      result = geopandas.GeoDataFrame(result, crs=4269)   
+      return result
     
-    if intersection:
+    selection = spatial_selector(states)
+
+    # Get the regions of interest (watersheds, zips, etc.) based on their intersection with the state(s)
+    if (region_type == "Census Tract"):
+      # Get all census tracts for this state
+      # Which state is it? FIPS look up
+      f = fips[states[0]] #assuming just one state for the time being
+      #print(f) # Debugging
+      # Get tracts
+      import requests, zipfile, io
+      url = "https://www2.census.gov/geo/tiger/TIGER2010/TRACT/2010/tl_2010_"+f+"_tract10.zip"
+      r = requests.get(url)
+      z = zipfile.ZipFile(io.BytesIO(r.content))
+      z.extractall("/content")
+      regions = geopandas.read_file("/content/tl_2010_"+f+"_tract10.shp")
+      regions.columns = regions.columns.str.lower() #convert columns to lowercase for consistency
+
+    else: 
+      #print(selection) # Debugging
       query = """
         SELECT this.* 
-        FROM """ + table_name + """ AS this 
-        JOIN """ + other.table_name + """ AS o 
-        ON o.""" + other.id_field + """ IN """ + selection + """ 
-        AND ST_Intersects(this.geom,o.geom) """
-    else:
-      query = """
+        FROM """ + spatial_tables[region_type]['table_name'] + """ AS this
+        JOIN """ + spatial_tables["State"]['table_name'] + """ AS other 
+        ON other.""" + spatial_tables["State"]['id_field'] + """ IN """ + selection + """ 
+        AND ST_Within(this.wkb_geometry,other.wkb_geometry) """
+      if region_filter:
+        query += """AND this.""" + spatial_tables[region_type]['match_field'] + """ = \'""" + region_filter + """\'"""
+
+      regions = retrieve(query)
+
+    # Get the intersecting geo (i.e. states)
+    query = """
       SELECT * 
-      FROM """ + table_name + """
-      WHERE """ + id_field + """ IN """ + selection + ""
+      FROM """ + spatial_tables["State"]['table_name'] + """
+      WHERE """ + spatial_tables["State"]['id_field'] + """ IN """ + selection + ""
+    states = retrieve(query) #reset intersecting_geo to its spatial data
 
-    #develop sql
-    sql = """
-      SELECT jsonb_build_object(
-          'type', 'FeatureCollection', 'features', jsonb_agg(features.feature)
-      )
-      FROM (
-          SELECT jsonb_build_object(
-              'type', 'Feature','id', gid, 'geometry',
-              ST_AsGeoJSON(geom)::jsonb,'properties',
-              to_jsonb(inputs) - 'gid' - 'geom'
-          ) feature
-          FROM ( 
-            """+query+"""
-          ) inputs
-      ) features;
-    """
-
-    url= 'http://portal.gss.stonybrook.edu/echoepa/index2.php?query=' 
-    data_location=url+urllib.parse.quote_plus(sql) + '&pg'
-    print(sql) # Debugging
-    print(data_location) # Debugging
-
-    result = geopandas.read_file(data_location)
-    return result
+    return regions, states
 
 # Read stored data from a file rather than go to the database.
 def read_file( base, type, state, region ):
