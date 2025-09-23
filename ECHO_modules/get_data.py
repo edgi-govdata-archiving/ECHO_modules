432690eb509380555519dcb6743eb3182f09685a
Eric Nost <ericnost@users.noreply.github.com>
2025-09-23 12:49:37 -0400

    Update get_data.py
    Fixes #56 
    Fixes #126 
    
    modifies get_data.py to daw on lookups in geographies.py

--- ECHO_modules/get_data.py
+++ ECHO_modules/get_data.py
@@ -31,23 +31,22 @@ def spatial_selector(units):
       selection = '(\''+str(units)+'\')'
     return selection
 
-def get_huc8_by_states(state_codes, json_file="data/state_huc8_map.json"):
+def get_huc8_by_states(state_codes):
     """
     Returns a combined list of HUC8 codes for the given list of state codes from the JSON mapping file.
     
     Parameters:
         state_codes (list): A list of state abbreviations (e.g., ['NY', 'NJ']).
-        json_file (str): The path to the JSON file containing the state-to-HUC8 mapping.
     
     Returns:
         list: A sorted list of unique HUC8 codes associated with the given state codes.
               Returns an empty list if none of the state codes are found.
     """
+    from ECHO_modules.geographies import huc8
     try:
-        with open(json_file, "r") as f:
-            state_huc8_map = json.load(f)
-    except FileNotFoundError:
-        print(f"Error: File '{json_file}' not found.")
+        state_huc8_map = huc8
+    except:
+        print(f"Error: HUC8 lookup not found.")
         return []
     
     # Use a set to avoid duplicate HUC8 codes.
@@ -61,19 +60,12 @@ def get_huc8_by_states(state_codes, json_file="data/state_huc8_map.json"):
     return sorted(list(combined_huc8))
 
 def state_abbr_to_fips(state_abbrs):
-    data_dict = {
-        "AL": "01","AK": "02","AZ": "04","AR": "05","CA": "06","CO": "08","CT": "09","DE": "10","DC": "11","FL": "12",
-        "GA": "13","HI": "15","ID": "16","IL": "17","IN": "18","IA": "19","KS": "20","KY": "21","LA": "22","ME": "23",
-        "MD": "24","MA": "25","MI": "26","MN": "27","MS": "28","MO": "29","MT": "30","NE": "31","NV": "32","NH": "33",
-        "NJ": "34","NM": "35","NY": "36","NC": "37","ND": "38","OH": "39","OK": "40","OR": "41","PA": "42","RI": "44",
-        "SC": "45","SD": "46","TN": "47","TX": "48","UT": "49","VT": "50","VA": "51","WA": "53","WV": "54","WI": "55",
-        "WY": "56","AS": "60","GU": "66","MP": "69","PR": "72","VI": "78"
-    }
+    from ECHO_modules.geographies import fips
     fips_codes = []
     for abbr in state_abbrs:
         # Check if the abbreviation is in the data dictionary's keys
         if abbr in data_dict:
-            fips_codes.append(data_dict[abbr]) # Append the corresponding FIPS code
+            fips_codes.append(fips[abbr]) # Append the corresponding FIPS code
         else:
             return None  # State abbreviation not found
     return fips_codes
